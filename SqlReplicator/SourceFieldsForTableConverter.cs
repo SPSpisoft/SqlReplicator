dd25e1813dfdb639d1c587542d92f6bde5a6a0ac
Spisoft <spring_software@yahoo.com>
2025-07-19 23:20:03 +0330

    before fix fileld loac

--- SqlReplicator/SourceFieldsForTableConverter.cs
+++ SqlReplicator/SourceFieldsForTableConverter.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace SqlReplicator
 {
-    public class SourceFieldsForTableConverter : IMultiValueConverter
+    public class SourceFieldsForTableConverter : IMultiValueConverter, IValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
@@ -20,6 +20,22 @@ namespace SqlReplicator
             }
             return new List<string>();
         }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var tables = value as IEnumerable<TableInfo>;
+            var tableName = parameter as string;
+            
+            if (tableName != null && tables != null)
+            {
+                var table = tables.FirstOrDefault(t => t.TableName == tableName);
+                if (table != null)
+                    return table.Fields.Select(f => f.FieldName).ToList();
+            }
+            return new List<string>();
+        }
+
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 } 
